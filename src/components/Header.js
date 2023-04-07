2a9800dc71f3c85ef4403c60d0636d6683862f2d
joselinecedano <118120312+joselinecedano@users.noreply.github.com>
2023-04-06 20:40:42 -0400

    header and footer componets created and linked

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+
+const Header = (props) => {
+    return(
+        <div className='header'>
+           <h1>This is the Header component!</h1> 
+        </div>
+    );
+};
+
+export default Header;
