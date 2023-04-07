5d8b74ea2c752dc37bec1c9424a0be3afa883560
joselinecedano <118120312+joselinecedano@users.noreply.github.com>
2023-04-07 00:18:43 -0400

    created .env file to store url, started working on index

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,10 +4,37 @@ import Index from "../pages/Index";
 import Show from "../pages/Show";
 
 const Main = (props) => {
+    //state to hold list of funds 
+    const [funds, setFunds] = useState(null);
+    //url for backend
+    const URL = process.env.REACT_APP_BACKEND_URL;
+    //function to make the api call 
+    const getFunds = async () => {
+        const response = await fetch(URL);
+        const data = await response.json();
+        setFunds(data);
+        console.log(data);
+    };
+    const createFund = async (fund) => {
+        //make post request to create a fund
+        await fetch(URL, {
+            method: "post",
+            headers:{
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(fund),
+        });
+        //update list of funds
+        getFunds();
+    };
     return(
         <div className='main'>
-            <h1> This is the Main component!</h1>
-             {/* this will store all of our funds at some point and once clicked will send user to funds show page with comments*/}
+             <Routes>
+                {/* route to hit index page of posted funds */}
+                <Route path="/jxfunds" element={<Index funds={funds} createFund={createFund}/>}/>
+                {/* route to hit show page of specific funds post */}
+                <Route path="/jxfunds/:id" element={<Show />}/>
+             </Routes>
         </div>
     );
 };
