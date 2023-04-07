3cfa611bce8f68cacce8f54301177f44a6d6241f
joselinecedano <118120312+joselinecedano@users.noreply.github.com>
2023-04-06 23:17:51 -0400

    created base for landing

---
+++ src/pages/Landing.js
@@ -0,0 +1,11 @@
+
+const Landing = (props) => {
+    return(
+        <div>
+            <h1> JX FUNDS</h1>
+            <p> There will maybe be a search bar here. </p>
+        </div>
+    );
+};
+
+export default Landing;
