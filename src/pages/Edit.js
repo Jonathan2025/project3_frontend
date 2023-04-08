9ff84840e46a4ab91ea30befead2a518ba6fda5f
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-08 15:44:05 -0400

    added state for the editForm in edit.js

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -14,17 +14,20 @@ const Edit = (props) => {
     const funds = props.funds.data 
     console.log("This is going to show the funds", funds) // when we console log funds we get the data array with the funds and their respective information
     
-    // f stands for fund 
+    // f stands for fund, this arrow function checks if the "_id" property of that fund is equal to the id in the url 
     const fund = funds.find((f) => f._id === id )
     console.log(fund) // we get the first fund information that MATCHES the id in the url
 
+    
+    // state for the form 
+    const [editForm, setEditForm] = useState(fund);
 
     return (
         <div className = "editPerson">
             <p>You reached the edit page</p>
         </div>
     )
-
+        
 
 }
 
