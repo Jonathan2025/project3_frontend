99e025b32e5bdbbfbbf0d297f121fd622e473527
joselinecedano <118120312+joselinecedano@users.noreply.github.com>
2023-04-08 11:13:17 -0400

    linked landing, took off search bar

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,7 +3,7 @@ const Landing = (props) => {
     return(
         <div>
             <h1> JX FUNDS</h1>
-            <p> There will maybe be a search bar here. </p>
+            <p> <button > JX FUNDS</button> </p>
         </div>
     );
 };
