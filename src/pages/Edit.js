ee2da2aa0cef857b299a847b7e76bea4d2e0faef
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-08 14:33:30 -0400

    added a simple boiler plate to edit.js for now and then we will add a form

---
+++ src/pages/Edit.js
@@ -0,0 +1,21 @@
+// import the necessary useParams, usestate etc
+import { useState } from "react";
+import { useParams, useNavigate } from "react-router-dom"
+
+
+// First step is just to create a basic boilerplate and access it 
+
+const Edit = (props) => {
+
+
+    return (
+        <div className = "editPerson">
+            <h1>You have reached the edit page where the edit page will be shown</h1>
+        </div>
+    )
+
+
+}
+
+
+export default Edit
