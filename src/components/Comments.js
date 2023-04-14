c307eae7cf127c1fcc6bdb496bb48930268d7983
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-14 12:16:48 -0400

    tested initialization for comet chat is successful so far

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -4,6 +4,8 @@ const appID = process.env.REACT_APP_COMETCHAT_APP_ID
 const region = process.env.REACT_APP_COMETCHAT_REGION
 const appSetting = new CometChat.AppSettingsBuilder().subscribePresenceForAllUsers().setRegion(region).build()
 
+
+// Test to see that the initialization works
 CometChat.init(appID, appSetting).then(
   () => {
     console.log("Initialization Completed Successfully")
