095c0ab9d83675969f7c604ad97b506bd32f83b3
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-14 17:53:22 -0400

    added in the starter code to initialize the comet chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,6 +1,21 @@
 import { CometChat } from "@cometchat-pro/chat";
 
-// initialize the Comet Chat 
+// initialize the Comet Chat
+const appID = process.env.REACT_APP_COMETCHAT_APP_ID
+const region = process.env.REACT_APP_COMETCHAT_REGION
+const appSetting = new CometChat.AppSettingsBuilder().subscribePresenceForAllUsers().setRegion(region).build();
+CometChat.init(appID, appSetting).then(
+  () => {
+    console.log("Initialization has been completed successfully");
+    // You can now call login function.
+  },
+  error => {
+    console.log("Initialization failed with error:", error);
+    // Check the reason for error and take appropriate action.
+  }
+);
+
+
 function Chat() {
     return (
         <h1>This will be the Chat section</h1>
