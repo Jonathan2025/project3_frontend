5ab71848bac8538957d9ced644374757828b3b17
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-14 17:46:59 -0400

    added a simple boiler plate for the chat.js

---
+++ src/components/Chat.js
@@ -0,0 +1,9 @@
+import { CometChat } from "@cometchat-pro/chat";
+
+// initialize the Comet Chat 
+function Chat() {
+    return (
+        <h1>This will be the Chat section</h1>
+    )
+}
+export default Chat
