4bc8d985716b037680af678be939370c73a09279
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-15 13:26:12 -0400

    added in the company logos, testing only fidelity and charles schwab for now, successfully showed on index page

--- src/components/CompanyLogo.js
+++ src/components/CompanyLogo.js
@@ -1,6 +1,18 @@
 const CompanyLogo = (props) => {
+
+    const companyLogoMap = {
+        "Fidelity": "https://logos-world.net/wp-content/uploads/2021/02/Fidelity-Symbol.jpg",
+        "Charles Schwab": "https://logos-world.net/wp-content/uploads/2021/03/Charles-Schwab-Emblem.png"
+
+    }
+
+    const companyName = props.fund.company
+    const logoSrc = companyLogoMap[companyName]
+
     return(
-        <h1>{props.fund.company}</h1>
+        <>
+        {logoSrc && <img src={logoSrc} alt={`${companyName} logo`} className="companyLogo"/>}
+        </>
     )
 }
 
