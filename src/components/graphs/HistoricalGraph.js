434e3f6e093f037c42d3586348496e53cad5a8a4
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-16 18:32:53 -0400

    able to access the monthly close data

--- src/components/graphs/HistoricalGraph.js
+++ src/components/graphs/HistoricalGraph.js
@@ -1,3 +1,23 @@
+import { useState, useEffect } from "react";
+import {Line} from 'react-chartjs-2'
+import {
+    Chart as ChartJS,
+    LineElement,
+    CategoryScale, // for x axis
+    LinearScale, // for the y axis
+    PointElement,
+    Legend, 
+    Tooltip
+} from 'chart.js'
+
+ChartJS.register(
+    LineElement,
+    CategoryScale,
+    LinearScale,
+    PointElement, 
+    Legend,
+    Tooltip
+)
 
 const HistoricalGraph = ({fund}) => {
 
@@ -5,7 +25,30 @@ const HistoricalGraph = ({fund}) => {
     const API_KEY = process.env.REACT_APP_API_KEY
     const symbol = fund.symbol
 
+    // Now we will get the URL of the API when ready please remove the comment for the first url. BUT for testing please use the second url
+    //const URL = `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=${symbol}&apikey=${API_KEY}`
+    const URL = `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=IBM&apikey=demo`
 
+
+    // Create a use state 
+    const [monthlyAdjustedCloseData, setMonthlyAdjustedCloseData] = useState(null)
+
+    //add the use effect 
+    // this useEffect will only run once the component mounts
+    useEffect(() => {
+        const getAPIData = async () => {
+            const response = await fetch(URL)
+            const data = await response.json()
+            console.log(data)
+            setMonthlyAdjustedCloseData(data)
+            
+
+            
+            }
+        getAPIData()
+    }, [])
+
+    console.log(monthlyAdjustedCloseData)
 
     return(
         <h1>This will be the Historical graph </h1>
--- src/components/graphs/PriceGraph.js
+++ src/components/graphs/PriceGraph.js
@@ -26,9 +26,11 @@ const PriceGraph = ({fund}) => {
   const symbol = fund.symbol
 
   // Now we will get the URL of the API when ready please remove the comment for the first url. BUT for testing please use the second url
-  //const URL = ADD IN THE ACTUAL URL WHEN READY
-  const URL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${symbol}&apikey=${API_KEY}`
-  
+  //const URL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${symbol}&apikey=${API_KEY}`
+  const URL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=demo`
+
+
+
   // Now we need to set up a state hook to our component and then state for the timeSeriesData
   const [fundAPIData, setFundAPIData] = useState(null)
   const [metaData, setMetaData] = useState(null)
