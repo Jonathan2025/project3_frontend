d997097da604ed5d39251730c66542a31fe17202
joselinecedano <joselinecedano0@gmail.com>
2023-04-20 19:19:39 -0400

    project styling done

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,23 +10,22 @@ const Header = (props) => {
       {/* this will hold our logo and nav bar */}
       <nav className="navBar ">
         <Link to="/about">
-          <div>About</div>
+          <div>Our Vision</div>
+        </Link>
+        <Link to='/blog'>
+          <div>Publications</div>
         </Link>
         <Link to="/jxfunds">
           <div>Funds</div>
         </Link>
-        <Link to="/jxfunds/create">
-          <div>Create</div>
-        </Link>
-
         <Link to='/' className='logo'>
           <div className='logo'>JxFUNDS</div>
         </Link>
         <div className="searchbar">
         <Search funds={funds} />
         </div>
-        <Link to='/blog'>
-          <div>Blog</div>
+        <Link to="/jxfunds/create">
+          <div> New Jx-Fund</div>
         </Link>
         <div className="dropDown">
            <button className="dropBtn">Account <span className="unicodeArrow">{`\u25BC`}</span></button>
