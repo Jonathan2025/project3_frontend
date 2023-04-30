017344f1570673d37415593fabfd7c9ab2c0c1e4
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-29 23:44:17 -0400

    removed the authentication from the about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,22 +1,5 @@
-import { Link } from "react-router-dom";
-import { useAuth0 } from '@auth0/auth0-react';
-import LoginButton from '../components/Authentication/Login';
 const About = () => {
-    const { isAuthenticated } = useAuth0();
-
-    if (!isAuthenticated) {
-        return (
-          <section className='createForm'>
-          <div>
-              <h1>Sign In to Access JXFunds!</h1>
-              <LoginButton className="loginButton"/>
-          </div>
-        </section>
-        );
-      }
-
-      
-
+    
     return(
         <> 
         <section className=" allAbout container-fluid">
