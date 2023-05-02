9d29fc75faf0d3383ce249a6922a34a31126524e
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-05-01 23:14:44 -0400

    slider - creating the arrows using react icons

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,10 +1,22 @@
-import React from "react";
-// import "./Slider.scss"
+import {useState} from "react";
+import "./Slider.scss"
+import {AiOutlineArrowLeft, AiOutlineArrowRight} from "react-icons/ai"
+import { sliderData } from "./Slider-data";
+
+
 
 const Slider = () => {
+
+    // create a use state
+    const [currentSlide, setCurrentSlide] = useState(0)
+
+
     return( 
-        <div> 
-            <h1>Slider</h1>
+        <div className="slider"> 
+            <AiOutlineArrowLeft className="arrow prev"/>
+            <AiOutlineArrowRight className="arrow next"/>
+
+
         </div>
     )
 }
